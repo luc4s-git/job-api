750f5fed7447f35fb27f675d55a86d5e0a886c67
luc4s-git <lucassantos4332@hotmail.com>
2023-10-03 16:44:37 -0300

    security middlewares

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // app
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
-const app = express();
+require('dotenv').config()
+require('express-async-errors')
+const express = require('express')
+const app = express()
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 
 // connectDb
 const connectDB = require('./db/connect')
@@ -13,30 +19,40 @@ const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
-app.use(express.json()); // so i can a access the req.body
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100 // limit each IP to 100 request per windowMs
+}))
+
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 // extra packages
 
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...\nAcess at: http://localhost:3000/`)
-    );
+    )
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-start();
+start()
